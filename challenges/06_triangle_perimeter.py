fee06421fd0a9ee63df7c3276a7e996a362305ce
James Blair <james@blair.nz>
2023-02-11 23:10:34 +1300

    n*n instead of n**2

--- challenges/06_triangle_perimeter.py
+++ challenges/06_triangle_perimeter.py
@@ -5,7 +5,7 @@ from input import get_int
 def main():
     base = get_int("please enter the base of the triangle: ")
     height = get_int("please enter the height of the triangle: ")
-    hypotenuse = sqrt(base**2+height**2)
+    hypotenuse = sqrt(base * base + height * height)  # multiplication is faster than exponentiation
     perimeter = base + height + hypotenuse
     print(f"the perimeter of that triangle is {perimeter:,} units")
 

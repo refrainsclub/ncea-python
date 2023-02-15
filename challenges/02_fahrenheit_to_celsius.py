ada211b91b8ecc4ecaba756b40d3428d021e1d81
jamesblair <james@jamesblair.nz>
2023-02-16 10:25:32 +1300

    round temperature to 2dp

--- challenges/02_fahrenheit_to_celsius.py
+++ challenges/02_fahrenheit_to_celsius.py
@@ -12,7 +12,7 @@ def main():
     fahrenheit_temp = get_int("enter a temperature in fahrenheit: ")
 
     celsius_temp = convert_to_celsius(fahrenheit_temp)
-    celsius_temp_rounded = round(celsius_temp)
+    celsius_temp_rounded = round(celsius_temp, 2)
 
     print(f"{fahrenheit_temp:,}°F is equal to {celsius_temp_rounded:,}°C")
 

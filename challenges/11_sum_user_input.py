dd3f55bd93cd8f4e5734baf8000348c1586e46a3
jamesblair <james@blair.nz>
2023-02-23 10:32:01 +1300

    make range inclusive

--- challenges/11_sum_user_input.py
+++ challenges/11_sum_user_input.py
@@ -5,8 +5,8 @@ def main():
     start = get_int("enter the start: ")
     end = get_int("enter the end: ")
     step = get_int("enter the steps: ")
-    total = sum(range(start, end, step))
-    print(f"the sum of all numbers in range({start:,}, {end:,}, {step:,}) is {total:,}")
+    total = sum(range(start, end + 1, step))
+    print(f"the sum of all numbers in that range is {total:,}")
 
 
 if __name__ == "__main__":

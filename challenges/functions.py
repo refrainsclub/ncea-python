8f4460cf793579a5f7e83422704569405c08ceca
jamesblair <james@blair.nz>
2023-03-21 15:01:49 +1300

    improve functions challenge

--- challenges/functions.py
+++ challenges/functions.py
@@ -1,26 +1,32 @@
 import turtle
 
 
-def draw_eye():
-    turtle.pendown()
-    turtle.pencolor("red")
+def draw_eye(x: float, y: float, color):
+    setup(x, y, color)
     turtle.circle(12)
     turtle.penup()
 
 
-def draw_outline():
-    turtle.pendown()
-    turtle.pencolor("blue")
+def draw_outline(x: float, y: float, color):
+    setup(x, y, color)
     turtle.circle(150)
     turtle.penup()
 
 
-turtle.penup()
-turtle.goto(-50, -150)
-draw_outline()
-turtle.goto(-150, 50)
-draw_eye()
-turtle.goto(50, 50)
-turtle.left(45)
-draw_eye()
-turtle.exitonclick()
+def setup(x: float, y: float, color):
+    turtle.goto(x, y)
+    turtle.pencolor(color)
+    turtle.pendown()
+
+
+def main():
+    turtle.speed(0)
+    turtle.penup()
+    draw_outline(-50, -150, "red")  # outline
+    draw_eye(-150, 50, "blue")  # first eye
+    draw_eye(50, 50, "purple")  # second eye
+    turtle.exitonclick()
+
+
+if __name__ == "__main__":
+    main()

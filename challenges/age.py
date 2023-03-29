6f94ad16cd087919c41eee6f689f37528bcb50f0
jamesblair <james@blair.nz>
2023-03-30 09:48:23 +1300

    extract age-getting process

--- challenges/age.py
+++ challenges/age.py
@@ -5,16 +5,22 @@ import input
 def main():
     birth_year = input.get_int("enter your birth year: ")
     birthday_passed = input.get_bool("have you had your birthday this year? (y/n): ")
-    year = get_year()
-    age = year - birth_year
-    if birthday_passed is False:
-        age -= 1
+    age = get_age(birth_year, birthday_passed)
+
     print(f"you are {age} years old")
 
 
 def get_year() -> int:
     date = datetime.datetime.now()
     return date.year
+
+
+def get_age(birth_year: int, birthday_passed: bool) -> int:
+    year = get_year()
+    age = year - birth_year
+    if birthday_passed is False:
+        age -= 1
+    return age
 
 
 if __name__ == '__main__':
